0f956cac576385774173157fa6bc11d8aac91b7e
BrenSeibel <brens.seibel@gmail.com>
2020-08-19 17:41:53 -0400

    done

---
+++ week1/math/app.js
@@ -0,0 +1,7 @@
+const math = require("./math.js")
+console.log(math)
+
+math.addition (2, 8) 
+math.substraction (5, 3)
+math.multiplication (2, 5)
+math.division (8, 2)
---
+++ week1/math/math.js
@@ -0,0 +1,17 @@
+function addition (num1, num2){
+    console.log(num1 + num2)
+}
+
+function substraction (num1, num2){
+    console.log(num1 - num2)
+}
+
+function multiplication (num1, num2){
+    console.log(num1 * num2)
+}
+
+function division (num1, num2){
+    console.log(num1 / num2)
+}
+
+module.exports = {addition, substraction, multiplication, division}
