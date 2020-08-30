16387e5d0e69ba8d93f259d1d9b7b12c78401d5f
BrenSeibel <brens.seibel@gmail.com>
2020-08-29 22:48:02 -0400

    working

--- week5/server.js
+++ week5/server.js
@@ -3,10 +3,41 @@ const app = express()
 
 app.use(Express.json)
 
-app.get("/", (request, response) => {
-    response.send({name: "brenda", age: 31})
-})
+app.get("/bountys", (req, res) => {
+    res.send({ bountys});
+});
 
+app.get("/bountys/:id", (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const bounty = bountys.find(bounty => bounty._id === id);
+    res.send(bounty);
+});
+
+app.post("/bountys", (req, res) => {
+    const newbounty = req.body;
+    newbounty._id = uuid.v4();
+    bountys.push(newbounty);
+
+    res.send(`Posted: Your new bounty has been added to the list of bountys`);
+});
+
+app.put("/bountys/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const index = bountys.findIndex(bounty => bounty._id === id);
+  const updatedbounty = Object.assign(bountys[index], req.body);
+  res.send(updatedbounty);
+});
+
+app.delete("/bountys/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const index = bountys.findIndex(bounty => bounty._id === id);
+  // const updatedbounty = Object.assign(bountys[index], req.body);
+  res.send(`Deleted entry`);
+  bountys.splice(index, 1);
+});
 
 app.listen(9000, () => {
     console.log("Working on port 9000")
