8b59147592256532489ded8b3e1352376dd16e7f
BrenSeibel <brens.seibel@gmail.com>
2020-09-09 21:14:53 -0400

    frontend

--- week5/client/src/App.js
+++ week5/client/src/App.js
@@ -7,20 +7,29 @@ class App extends React.Component {
   constructor (){
     super()
     this.state = {
-
+        array : []
     }
   } 
   
   componentDidMount(){
-    axios.get("/bounty3")
+    axios.get("/bountys")
     .then(response => {console.log(response.data)
-      this,setState({array: response.data})})
+      this.setState({array: response.data})})
     .catch(error => console.log(error))  
   }
   render (){
     return (
       <div className = "App">
+          {this.state.array.map((bounty, index) => {
 
+            return(
+              <div>
+                <h1>{bounty.name}</h1>
+                <p>{bounty.description}</p>
+                <img src = {bounty.imgUrl}/>
+              </div>
+            )
+          })}
       </div>
     )
   }
